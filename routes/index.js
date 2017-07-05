b442dc4da4c2ebef83f5282f7ad60e19f5bb606d
Michael Harvey <michaelharvey@Totoro.local>
2017-07-05 12:45:16 -0600

    added a hello .pug file

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const mike = {name:'mike'};
+  // res.json(mike)
+  // res.send('Hey! It works!');
+  res.render('hello', mike)
 });
 
+router.get('/reverse/:name',(req,res)=>{
+
+   const reverse = [...req.params.name].reverse().join('');
+     res.send(reverse);
+})
 module.exports = router;
